e00a15ebe80f1598a06035ea83cbabc2cd774003
Simonwristel <Simon.wristel@elev.ga.ntig.se>
2023-08-22 09:55:07 +0200

    modul 1

---
+++ modul 1.py
@@ -0,0 +1,15 @@
+print("Hello world")
+print("Viggo säger\" Ut med Allah\"")
+print("""
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+Lorem Ipsum
+""")
+print("Hej jag heter Måns \"Måns heter jag, Vad heter du?\"" )
