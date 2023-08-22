f4f4ebb8efcc6d2210092772d87399b5569ce8cd
Simonwristel <Simon.wristel@elev.ga.ntig.se>
2023-08-22 09:55:30 +0200

    d

---
+++ Untitled-2.py
@@ -0,0 +1,25 @@
+from typing import AsyncGenerator
+
+
+print("Hello world - jag är ny")
+
+print(20 + 20)
+print(20 * 20)
+print(20 / 20)
+print(20 - 20)
+#print(10 / 3)
+#print("10 // 3")
+
+#x= input("namn:")
+#age = input("ålder:")
+
+#print(x)
+#print(age)
+
+#print("Hej", x, "du är", age, "år")
+
+print("""
+Hej Klassen
+Detta är flerradig print""")
+
+print("resultat: \ teinf22b vann")
